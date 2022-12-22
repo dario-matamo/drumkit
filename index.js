c50a11b0c716091ef970e1bd43ed40f45294fdeb
dario-matamo <dario.matamo17@gmail..com>
2022-12-22 14:06:17 -0600

    Added background images to buttons and sounds to every button on click

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 const buttons = document.querySelectorAll(".drum");
 
-buttons.forEach(button => {
-    button.addEventListener("click", handleClick)
+buttons.forEach((button) => {
+	button.addEventListener("click", handleClick);
 });
 
 function handleClick() {
-    alert("Clicked");
+	var buttonText = this.textContent;
+	switch (buttonText) {
+		case "w":
+			var audio = new Audio("sounds/tom-1.mp3");
+			audio.play();
+			break;
+		case "a":
+			var audio = new Audio("sounds/tom-2.mp3");
+			audio.play();
+			break;
+		case "s":
+			var audio = new Audio("sounds/tom-3.mp3");
+			audio.play();
+			break;
+		case "d":
+			var audio = new Audio("sounds/tom-4.mp3");
+			audio.play();
+			break;
+		case "j":
+			var audio = new Audio("sounds/kick-bass.mp3");
+			audio.play();
+			break;
+		case "k":
+			var audio = new Audio("sounds/crash.mp3");
+			audio.play();
+			break;
+		case "l":
+			var audio = new Audio("sounds/snare.mp3");
+			audio.play();
+			break;
+		default:
+			console.log(this);
+	}
+	this.style = "color:red";
+	console.log(this.innerHTML);
 }
